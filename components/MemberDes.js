9d3cd2698af78b93bf0b99f93d79d269cc60a93d
shAtud <sidalihouda.computerscience@gmail.com>
2022-03-18 23:49:00 +0100

    fixing the footer

--- components/MemberDes.js
+++ components/MemberDes.js
@@ -4,32 +4,30 @@ import { membersData } from '../data';
 const MemberDes =(props) =>{
   
     return(
-        <div className='font-fontom' >
+        <div className='font-fontom border-2 flex flex-col' >
           {membersData.map((ele,index)=>{
             return(
               <div 
-                className={`flex items-center justify-center lg:h-[100vh] h-full w-[100%] ${index %2 ===0 ?'flex-row':'flex-row-reverse'}`}
+                className={`flex  justify-center items-center  h-[100vh] w-[100%] ${index %2 ===0 ?'flex-row':'flex-row-reverse'}`}
                 key={index}
                 id={'profil'+index}
                 >
               <div className= {`lg:flex hidden basis-2/3 item center ${index %2 ===0 ?'ml-16':'mr-16'} ${index %2 ===0 ?'justify-start':'justify-end'}`}>
                 <img src='/mem.png' height={800} width={900} className='object-contain'/>
               </div>
-              <div className='relative mx-24 my-20 h-[600px] lg:w-[500px] lg:max-w-[500px] max-w-min min-w-[390px] rounded-[15px] shadow-white shadow-2xl bg-white flex flex-col basis-1/3 items-center justify-center'>
-                    <div className='flex flex-row space-x-6 mt-6 ml-6'>
-                      <div className=''>
-                        <img src={ele.url} height={75} width={75} className='rounded-full'/>
-                      </div>
-                    <div className='flex flex-col'>
-                      <div className='text-[#5C4CFC] pr-10 text-[38px]'>{ele.fullName}</div>
-                      <div className='text-[#5C4CFC]/60 text-[25px]'>{ele.Email}</div>
+              <div className='relative mx-24  h-fit lg:w-[500px] lg:max-w-[500px] max-w-min min-w-[390px] rounded-[15px] shadow-white shadow-2xl bg-white flex flex-col basis-1/3 items-center '>
+                    <div className='flex  space-x-6  items-center '>
+                        <img src={ele.url}  className='rounded-full w-[60px] h-[60px] object-contain'/>
+                        <div className='flex flex-col'>
+                            <div className='text-[#5C4CFC] pr-10 text-[30px]'>{ele.fullName}</div>
+                            <div className='text-[#5C4CFC]/60 text-[25px]'>{ele.Email}</div>
+                        </div>
                     </div>
-                    </div>
-                    <div className='flex flex-col mt-10 ml-6'>
+                    <div className='flex flex-col mt-5 ml-6'>
                       <div className='text-[#5C4CFC] text-[25px] mb-1'>A propo de moi :</div>
-                      <div className='pr-10 text-[22px]'>{ele.propo}</div>
+                      <div className='pr-10 text-[18px]'>{ele.propo}</div>
                       <div className='text-[#5C4CFC] text-[25px] mb-1 mt-4'>Bio technique :</div>
-                      <div className='pr-10 text-[22px]'>{ele.bio}</div>
+                      <div className='pr-10 text-[18px]'>{ele.bio}</div>
                       <div className='flex flex-row mt-4'>
                         <div className='text-[#5C4CFC] text-[25px]'>Tlf/WhatsApp :</div>  
                         <div className='text-[#5C4CFC]/60 text-[22px]'>{ele.number}</div>
--- components/Ser.js
+++ components/Ser.js
@@ -17,15 +17,18 @@ const Ser = props =>{
         ]
 
     return(
-        <div className='lg:h-[100vh] h-full pb-32 w-full md:w-[100%] font-fontom bg-[#F7FAFB]' id='services_section'>
-          <div className='pt-16 ml-12 text-6xl mb-28 text-[#5C4CFC]'>Services</div>
-            <div className="gap-24 flex flex-row flex-wrap place-items-center justify-center">
+        <div 
+          className='border-2  h-[100vh]  w-full md:w-[100%] font-fontom bg-[#F7FAFB] flex flex-col space-y-8' 
+          id='services_section'
+          >
+          <div className='ml-12 text-6xl  text-[#5C4CFC]'>Services</div>
+            <div className=" flex flex-row flex-wrap space-x-12 justify-center">
                     {comp.map((el,index)=>{
                         return(
                         <div key={index} className="bg-[#C1CBFF]/80 min-w-[350px] h-[400px] w-[350px] rounded-[25px] shadow-xl shadow-stone-300 text-center flex-col flex items-center">
-                        <div className='text-[25px] text-center mt-6 h-[100px]'>{el.title}</div>
-                        <div className='h-[300px] w-[300px] flex justify-center'>
-                        <img src={el.url} className='object-contain'/>
+                            <div className='text-[25px] text-center mt-6 h-[100px]'>{el.title}</div>
+                            <div className='h-[300px] w-[300px] flex justify-center'>
+                            <img src={el.url} className='object-contain'/>
                         </div>
                     </div>
                         )
